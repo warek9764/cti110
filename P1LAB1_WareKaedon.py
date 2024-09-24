37c0b3bab9b4fcccfc0fadd4318dbf30ccb40a84
warek9764 <warek9764@student.faytechcc.edu>
2024-09-24 11:30:08 -0400

    Add files via upload

---
+++ P1LAB1_WareKaedon.py
@@ -0,0 +1,16 @@
+#Kaedon Ware
+#9/24/24
+#P1LAB1
+#Shows basic inputs for Python
+
+#Get first name of person
+firstname = input("Enter your first name: ")
+print()
+
+
+#Get last name of person
+lastname = input("Enter your last name: ")
+print()
+
+
+print(f"Hello, {firstname} {lastname}! Welcome to CTI-110")
